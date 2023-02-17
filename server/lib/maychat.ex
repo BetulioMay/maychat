74cffae6049676b20ae0d180ffd7f4c4eed01240
Cesar Mayora <cesarmay@protonmail.com>
2023-02-17 12:39:18 +0100

    chore: Logging port on server start

--- server/lib/maychat.ex
+++ server/lib/maychat.ex
@@ -2,19 +2,23 @@ defmodule Maychat do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
+    port = get_cowboy_port(Application.get_env(:maychat, :port))
+
     children = [
       Maychat.Repo,
-      {Plug.Cowboy,
-       scheme: :http,
-       plug: MaychatWeb.Router,
-       options: [port: get_cowboy_port(Application.get_env(:maychat, :port))]}
+      {Plug.Cowboy, scheme: :http, plug: MaychatWeb.Router, options: [port: port]}
     ]
 
     opts = [strategy: :one_for_one, name: Maychat.Supervisor]
-    Supervisor.start_link(children, opts)
+    sup = Supervisor.start_link(children, opts)
+
+    Logger.info("Listening on port #{port}...")
+
+    sup
   end
 
   defp get_cowboy_port(port) when is_binary(port) and port != "",

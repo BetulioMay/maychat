f67ab3832bdf3ac748c6f8a0c4af4d11be5d97cc
Cesar Mayora <cesarmay@protonmail.com>
2023-02-18 10:56:32 +0100

    fix: import with relative path

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const LoginForm = dynamic(() => import("@/components/forms/LoginForm"), {
+const LoginForm = dynamic(() => import("components/forms/LoginForm"), {
   ssr: false,
 });
 

7b5a3c164aa48ffb29a24dfae604fa23272e0beb
Cesar Mayora <cesarmay@protonmail.com>
2023-02-23 21:29:01 +0100

    chore: minimal changes

---
+++ server/test/maychat_web/plugs/response_case_test.exs
@@ -0,0 +1,10 @@
+defmodule MaychatWeb.Plugs.ResponseCaseTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  alias Recase.CamelCase
+  alias Recase.SnakeCase
+  alias MaychatWeb.Plugs.ResponseCase
+
+  # TODO: find a way to test this plug
+end

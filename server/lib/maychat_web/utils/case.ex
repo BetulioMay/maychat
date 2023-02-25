e6aa75ccf6edf6d68d5ad86d74cc774e4aed9b59
Cesar Mayora <cesarmay@protonmail.com>
2023-02-25 18:29:13 +0100

    fix: case values is values is map (JSON)

--- server/lib/maychat_web/utils/case.ex
+++ server/lib/maychat_web/utils/case.ex
@@ -6,16 +6,22 @@ defmodule MaychatWeb.Utils.Case do
   def call(body_params, converter)
 
   def call(body_params, converter) when converter in @converters do
-    for {k, v} <- body_params, into: %{} do
-      {
-        converter.convert(k),
-        v
-      }
-    end
+    do_case(body_params, converter)
   end
 
   # NOTE: actually, if PascalCase is passed, it is supported
   # Nevertheless, it's desired to be sure that the converters that we want
   # are passed.
   def call(_, conv), do: raise(ArgumentError, "converter #{inspect(conv)} is not supported")
+
+  defp do_case(body_params, converter) when is_map(body_params) do
+    for {k, v} <- body_params, into: %{} do
+      {
+        converter.convert(k),
+        do_case(v, converter)
+      }
+    end
+  end
+
+  defp do_case(value, _), do: value
 end
--- server/test/maychat_web/plugs/response_case_test.exs
+++ server/test/maychat_web/plugs/response_case_test.exs
@@ -1,10 +1,10 @@
 defmodule MaychatWeb.Plugs.ResponseCaseTest do
-  use ExUnit.Case, async: true
-  use Plug.Test
+  # use ExUnit.Case, async: true
+  # use Plug.Test
 
-  alias Recase.CamelCase
-  alias Recase.SnakeCase
-  alias MaychatWeb.Plugs.ResponseCase
+  # alias Recase.CamelCase
+  # alias Recase.SnakeCase
+  # alias MaychatWeb.Plugs.ResponseCase
 
   # TODO: find a way to test this plug
 end
